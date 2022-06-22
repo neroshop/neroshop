afd2ed6437e440e07741de8c2bf253cda8310e14
larteyoh <58671384+larteyoh@users.noreply.github.com>
2022-06-22 09:22:03 +0000

    Rewrite main.cpp, add lmdb header and update to C++17 (#9)
    * Rewrite main.cpp
    
    * Added lmdb as external dependency
    
    * Fixed .gitmodules
    
    * Revert "Fixed .gitmodules"
    
    This reverts commit 0e2c874239e0c22efdbf5f99d243dd43f54248b6.
    
    * Update readme and other minor changes
    
    * Removed lmdb from readme and .gitmodules
    
    * Add src/database/lmdb.hpp
    
    * Update C++ to 17

--- include/database.hpp
+++ include/database.hpp
@@ -1,7 +1,7 @@
 #ifndef DATABASE_HPP_NEROSHOP // recommended to add unique identifier like _NEROSHOP to avoid naming collision with other libraries
 #define DATABASE_HPP_NEROSHOP
 
-//#include "" // non-relational or NoSQL
+#include "database/lmdb.hpp" // non-relational or NoSQL
 #if defined(NEROSHOP_USE_POSTGRESQL) 
 #include "database/postgresql.hpp" // relational (server-based)
 #endif
