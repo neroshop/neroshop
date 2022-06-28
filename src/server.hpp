a0e48f0d9ee0a6dfb48dab3bf0312d5b46b7ad7e
Jozsef Bakosi <jbakosi@protonmail.com>
2022-06-28 11:58:12 +0100

    Cosmetics cleanup daemon code

--- src/server.hpp
+++ src/server.hpp
@@ -1,5 +1,4 @@
-#ifndef SERVER_HPP_NEROSHOP
-#define SERVER_HPP_NEROSHOP
+#pragma once
 
 #include <iostream>
 #include <cstring>
@@ -13,41 +12,40 @@
 #include "debug.hpp"
 
 namespace neroshop {
-class Server { // listens to a port and accepts a socket
-public:
+
+class Server {
+  public:
     Server();
-	~Server();
-	bool bind(unsigned int port);
-	bool listen();
-	bool accept();
-	bool accept_all();
-	void write(const std::string& text);
-	std::string read();
-	void close(); // closes socket
-	void shutdown(); // shuts down entire connection, ending receiving and sending
-	//void check_for_internet(); // server needs to make sure there is internet so that it can access database
-	//InternetGetConnectedState();//win32
-	// getters
-	//std::string get_address() const;
-	//unsigned int get_port() const;
-#ifdef __gnu_linux__
-	int get_socket() const;
-	int get_client_socket(int index = 0) const;
-#endif
-private:
-#ifdef _WIN32
-	SOCKET socket, newsocket;
-	char buffer[512];
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t clilen;
-#endif
-#ifdef __gnu_linux__
-	int socket, newsocket; // server-socket and client-socket //std::vector<int> newsockets;// temp
-	char buffer[512]/*[256]*/; std::string buffer_new;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t clilen;
-#endif	
-	unsigned int port;
+    ~Server();
+
+    bool bind( unsigned int port );
+    bool listen();
+    bool accept();
+    void write( const std::string& text );
+    std::string read();
+    void close();
+    void shutdown();
+
+    #ifdef __gnu_linux__
+    int get_socket() const { return socket; }
+    int get_client_socket(int index = 0) const { return newsocket; }
+    #endif
+
+  private:
+    #ifdef _WIN32
+    SOCKET socket, newsocket;
+    char buffer[512];
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t clilen;
+    #endif
+    #ifdef __gnu_linux__
+    int socket, newsocket;
+    char buffer[512];
+    std::string buffer_new;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t clilen;
+    #endif
+    unsigned int port;
 };
-}
-#endif
+
+} // neroshop::
